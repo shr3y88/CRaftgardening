407ce35d63eca51040e9d663d863bbf0bf18e29d
shr3y88 <shr3yanshtiwari@gmail.com>
2025-09-10 15:58:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv").config();
 const connectDb = require("./config/connectionDb");
 const cors = require("cors");
-
 
 const plantRoutes = require("./routes/plantRoutes");
 const participantRoutes = require("./routes/participantRoutes");
 const userRoutes = require("./routes/user");
 const recipeRoutes = require("./routes/recipe");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 
@@ -17,27 +16,26 @@ connectDb();
 
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",          
-      "http://localhost:3000",            
-      "https://c-raftgardening.vercel.app" 
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://c-raftgardening.vercel.app",
     ],
     credentials: true,
   })
 );
 
-
 app.use("/images", express.static("public/images"));
 
 
 app.use("/api/users", userRoutes);       
-app.use("/api/recipes", recipeRoutes);  
-app.use("/api/plants", plantRoutes);     
-app.use("/api/participants", participantRoutes);
+app.use("/api/recipes", recipeRoutes);     
+app.use("/api/plants", plantRoutes);       
+app.use("/api/participants", participantRoutes); 
 
 
 app.use((err, req, res, next) => {
@@ -46,11 +44,7 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(PORT, (err) => {
-  if (err) {
-    console.error("Error starting server:", err);
-  } else {
-    console.log(` Server running on port ${PORT}`);
-  }
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
