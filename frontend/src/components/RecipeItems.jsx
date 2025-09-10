55c7eb217f8a64da51d173963e0fdefd7dfb94de
shr3y88 <shr3yanshtiwari@gmail.com>
2025-09-10 15:23:22 +0530

    Update RecipeItems.jsx

--- frontend/src/components/RecipeItems.jsx
+++ frontend/src/components/RecipeItems.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useLoaderData, useNavigate } from 'react-router-dom'
-
 import { MdDateRange } from "react-icons/md";
 import { FaHeart } from "react-icons/fa6";
 import { FaEdit } from "react-icons/fa";
@@ -8,63 +7,97 @@ import { MdDelete } from "react-icons/md";
 import axios from 'axios';
 
 export default function RecipeItems() {
-    const recipes = useLoaderData()
-    const [allRecipes, setAllRecipes] = useState()
-    let path = window.location.pathname === "/Events" ? true : false
-    let favItems = JSON.parse(localStorage.getItem("fav")) ?? []
-    const [isFavRecipe, setIsFavRecipe] = useState(false)
-    const navigate=useNavigate()
-    const API_BASE = "https://craftgardening-3.onrender.com";
-    console.log(allRecipes)
+  const { recipes } = useLoaderData(); // from loader in App.js
+  const [allRecipes, setAllRecipes] = useState([]);
+  const navigate = useNavigate();
+  const API_BASE = "https://craftgardening-3.onrender.com";
 
-    useEffect(() => {
-        setAllRecipes(recipes)
-    }, [recipes])
+  // path check: are we in My Events?
+  const isMyEventsPage = window.location.pathname === "/my-events";
 
-    const onDelete = async (id) => {
-        await axios.delete(`${API_BASE}/recipe/${id}`)
-            .then((res) => console.log(res))
-        setAllRecipes(recipes => recipes.filter(recipe => recipe._id !== id))
-        let filterItem = favItems.filter(recipe => recipe._id !== id)
-        localStorage.setItem("fav", JSON.stringify(filterItem))
+  let favItems = JSON.parse(localStorage.getItem("fav")) ?? [];
+
+  useEffect(() => {
+    setAllRecipes(recipes || []);
+  }, [recipes]);
+
+  const onDelete = async (id) => {
+    try {
+      const token = localStorage.getItem("token");
+      await axios.delete(`${API_BASE}/recipe/${id}`, {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+
+      setAllRecipes((prev) => prev.filter((recipe) => recipe._id !== id));
+      let filterItem = favItems.filter((recipe) => recipe._id !== id);
+      localStorage.setItem("fav", JSON.stringify(filterItem));
+    } catch (err) {
+      console.error("Delete failed:", err);
+      alert("Failed to delete recipe.");
+    }
+  };
+
+  const favRecipe = (item) => {
+    const exists = favItems.some((recipe) => recipe._id === item._id);
+    let updatedFavs;
+
+    if (exists) {
+      updatedFavs = favItems.filter((recipe) => recipe._id !== item._id);
+    } else {
+      updatedFavs = [...favItems, item];
     }
 
-    const favRecipe = (item) => {
-        let filterItem = favItems.filter(recipe => recipe._id !== item._id)
-        favItems = favItems.filter(recipe => recipe._id === item._id).length === 0 ? [...favItems, item] : filterItem
-        localStorage.setItem("fav", JSON.stringify(favItems))
-        setIsFavRecipe(pre => !pre)
-    }
+    localStorage.setItem("fav", JSON.stringify(updatedFavs));
+    favItems = updatedFavs;
+    // trigger rerender
+    setAllRecipes([...allRecipes]);
+  };
 
-    return (
-        <>
-            <div className='card-container'>
-                {
-                    allRecipes?.map((item, index) => {
-                        return (
-                            <div key={index} className='card'onDoubleClick={()=>navigate(`/recipe/${item._id}`)}>
-                                <img src={`http://localhost:5000/images/${item.coverImage}`} width="120px" height="100px"></img>
-                                <div className='card-body'>
-                                    <div className='title'>{item.title}</div>
-                                    <div className='icons'>
-                                        <div className='timer'><MdDateRange />{item.time}</div>
-                                        {(!path) ? <FaHeart onClick={() => favRecipe(item)}
-                                            style={{ color: (favItems.some(res => res._id === item._id)) ? "red" : "" }} /> :
-                                            <div className='action'>
-                                                <Link to={`/editRecipe/${item._id}`} className="editIcon"><FaEdit /></Link>
-                                                <MdDelete onClick={() => onDelete(item._id)} className='deleteIcon' />
-                                            </div>
-                                        }
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+  return (
+    <div className='card-container'>
+      {allRecipes?.map((item) => (
+        <div
+          key={item._id}
+          className='card'
+          onDoubleClick={() => navigate(`/recipe/${item._id}`)}
+        >
+          <img
+            src={`${API_BASE}/images/${item.coverImage}`}
+            width="120px"
+            height="100px"
+            alt={item.title}
+          />
+          <div className='card-body'>
+            <div className='title'>{item.title}</div>
+            <div className='icons'>
+              <div className='timer'>
+                <MdDateRange /> {item.time}
+              </div>
+              {!isMyEventsPage ? (
+                <FaHeart
+                  onClick={() => favRecipe(item)}
+                  style={{
+                    color: favItems.some((res) => res._id === item._id)
+                      ? "red"
+                      : ""
+                  }}
+                />
+              ) : (
+                <div className='action'>
+                  <Link to={`/editRecipe/${item._id}`} className="editIcon">
+                    <FaEdit />
+                  </Link>
+                  <MdDelete
+                    onClick={() => onDelete(item._id)}
+                    className='deleteIcon'
+                  />
+                </div>
+              )}
             </div>
-        </>
-    )
-
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
-
 
