d4debb1f51a0f250459faca1f5794fa42d08ff3b
shr3y88 <shr3yanshtiwari@gmail.com>
2025-09-11 17:18:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,8 +32,8 @@ app.use(
 app.use("/images", express.static("public/images"));
 
 
-app.use("/api/users", userRoutes);       
-app.use("/api/recipe", recipeRoutes);     
+app.use("/api/users", user);       
+app.use("/api/recipe", recipe);     
 app.use("/api", plantRoutes);       
 app.use("/api/participants", participantRoutes); 
 
@@ -47,6 +47,7 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
