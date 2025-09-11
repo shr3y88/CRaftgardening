330afa2a40aeb2c4135f3322fe8bc7bfe4416fb7
shr3y88 <shr3yanshtiwari@gmail.com>
2025-09-11 21:42:19 +0530

    Delete backend directory

--- backend/config/connectionDb.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose")
-
-const connectDb=async()=>{
-    await mongoose.connect(process.env.CONNECTION_STRING)
-    .then(()=>console.log("connected..."))
-}
-
-module.exports=connectDb
